19e5b6c0d2b7589418ef8844e04e53d106b5ff67
Duncan Gibson <legowerewolf@gmail.com>
2023-01-13 16:35:50 -0500

    orchestrate crawling better

--- scraper.go
+++ scraper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -70,27 +71,18 @@ func main() {
 	log.Println("Running at", seedURL.String(), "across", strconv.Itoa(pages), "pages with a", strconv.Itoa(delay), "second delay and series set to", strconv.FormatBool(includeSeries))
 
 	// Channels
+	queue := make(chan string, 10*pages)
 	chworks := make(chan string)
 	chseries := make(chan string)
 	finished := make(chan bool)
+
+	go fill_queue(queue, delay, seedURL, pages)
+	go crawl_queue(queue, delay, chworks, chseries, finished)
 
 	// Crawl the listing - with pagination
 	bar := pb.New(pages)
 	if showProgress {
 		bar.Start()
-	}
-
-	for page := 1; page <= pages; page++ {
-
-		query := seedURL.Query()
-		query.Set("page", strconv.Itoa(page))
-		seedURL.RawQuery = query.Encode()
-
-		go crawl(seedURL.String(), chworks, chseries, finished)
-
-		if page < pages {
-			time.Sleep(time.Duration(delay) * time.Second)
-		}
 	}
 
 	foundWorks := make(map[string]bool)
@@ -106,7 +98,8 @@ func main() {
 				continue
 			} else {
 				foundSeries[url] = true
-				go crawl(url, chworks, chseries, finished)
+				log.Println("Found series", url)
+				queue <- url
 				addlPages++
 				bar.SetTotal(pages + addlPages)
 			}
@@ -116,11 +109,30 @@ func main() {
 		}
 	}
 
-	bar.FinishPrint("Finished crawling!")
+	bar.Finish()
 
 	fmt.Println("\nFound", len(foundWorks), "works across", pages, "pages and", len(foundSeries), "series:\n ")
 	for url := range foundWorks {
 		fmt.Println(toFullURL(url))
+	}
+}
+
+func fill_queue(queue chan string, delay int, seedURL *url.URL, pages int) {
+	for page := 1; page <= pages; page++ {
+
+		query := seedURL.Query()
+		query.Set("page", strconv.Itoa(page))
+		seedURL.RawQuery = query.Encode()
+
+		queue <- seedURL.String()
+	}
+	log.Println("Filled queue with", pages, "pages")
+}
+
+func crawl_queue(queue chan string, delay int, works chan string, series chan string, finished chan bool) {
+	for url := range queue {
+		go crawl(url, works, series, finished)
+		time.Sleep(time.Duration(delay) * time.Second)
 	}
 }
 
@@ -154,33 +166,33 @@ func crawl(url string, works chan string, series chan string, chFinished chan bo
 				continue
 			}
 
-			ok, url := getHref(t)
-			if !ok {
+			href, err := getHref(t)
+			if err != nil {
 				continue
 			}
 
-			isWork, _ := regexp.MatchString("/works/\\d+", url)
-			isSeries, _ := regexp.MatchString("/series/\\d+", url)
-			isSpecial, _ := regexp.MatchString("bookmarks|comments|collections|search|tags|users|transformative|chapters", url)
+			isWork, _ := regexp.MatchString("/works/\\d+", href)
+			isSeries, _ := regexp.MatchString("/series/\\d+", href)
+			isSpecial, _ := regexp.MatchString("bookmarks|comments|collections|search|tags|users|transformative|chapters", href)
 			if isWork && !isSpecial {
-				works <- url
+				works <- href
 			}
-			if isSeries && !isSpecial {
-				series <- url
+			if isSeries && !isSpecial && !strings.Contains(url, "series") {
+				series <- href
 			}
 
 		}
 	}
 }
 
-func getHref(t html.Token) (ok bool, href string) {
+func getHref(t html.Token) (string, error) {
 	for _, a := range t.Attr {
 		if a.Key == "href" {
-			href = a.Val
-			ok = true
+			return a.Val, nil
+
 		}
 	}
-	return
+	return "", errors.New("no href attribute found")
 }
 
 func toFullURL(url string) string {
