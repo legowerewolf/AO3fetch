df75aa321ddc8b4fa1a3f9cfcd35dd5562ac9ff3
Duncan Gibson <duncan@legowerewolf.net>
2025-05-28 21:45:30 -0400

    fuzz the write function

--- logbuffer/logbuffer_test.go
+++ logbuffer/logbuffer_test.go
@@ -5,6 +5,22 @@ import (
 	"testing"
 )
 
+func FuzzWrite(f *testing.F) {
+	f.Add("Hello, World!")
+	f.Add("Hello, World!\nLine2\nLine3")
+
+	f.Fuzz(func(t *testing.T, s string) {
+		expectedLineCount := strings.Count(s, "\n") + 1
+
+		buf := NewLogBuffer()
+		buf.Write([]byte(s))
+
+		if len(*buf.stream) != expectedLineCount {
+			t.Fail()
+		}
+	})
+}
+
 func FuzzGetAtMostFromEnd(f *testing.F) {
 	f.Add("Hello, World!", 1)
 	f.Add("Hello, World!", 10)
@@ -19,7 +35,6 @@ func FuzzGetAtMostFromEnd(f *testing.F) {
 		lines := buf.GetAtMostFromEnd(l)
 
 		if len(lines) != expectedLineCount {
-
 			t.Fail()
 		}
 	})
